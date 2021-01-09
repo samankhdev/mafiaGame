66923c8a42eef29c2cdcfb5032194d6c850a4ffd
samankhdev <samankh.dev@gmail.com>
2021-01-10 01:52:48 +0330

    rule only 6 user can play.

--- src/components/ShowNames.vue
+++ src/components/ShowNames.vue
@@ -8,15 +8,13 @@
             <ul>
                 <li class="text-danger">شما:  {{YourName}}</li>
                 <li v-for="name in names" :key="name.id">
-
-{{name.name}}
-
+                    {{name.name}}
 
                 </li>
             </ul>
       </div>
       <div class="card card-footer">
-          <button class="btn btn-outline-danger">بعدی</button>
+          <button class="btn btn-outline-danger" @click="next">بعدی</button>
       </div>
   </div>
 </template>
@@ -30,20 +28,17 @@ data(){
             {id:2,name:"محمد پیوندی"},
             {id:3,name:"سارا میرشکاری"},
             {id:4,name:"مهدی رسولی"},
-            {id:5,name:"رضا پیوندی"},
-            {id:6,name:"سینا"},
-            {id:7,name:"حسین"},
-            {id:8,name:"محسن"},
-            {id:9,name:"سلمان"},
-            {id:10,name:"قاسم"},
-            {id:11,name:"غلامرضا"},
-            {id:12,name:"صادق گنجی"},
-            {id:13,name:"مختار باهنر"},
-            {id:14,name:"محمد کشتکار"},
-            {id:15,name:"علی محمدی"}
+            {id:5,name:"رضا پیوندی"}
         
         ],
-        YourName:''
+        YourName:'',
+        array:[],
+        times:5
+    }
+},
+methods:{
+    next(){
+        window.location.replace("/select-jobs")
     }
 },
 computed:{
@@ -51,7 +46,8 @@ computed:{
             //get Name
             var name=localStorage.getItem("username").toString();
             this.YourName=name;
-    }
+    },
+   
 }
 }
 </script>
