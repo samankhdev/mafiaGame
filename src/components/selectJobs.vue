4de091177f9053118b38796b880530d1c8a62f60
samankhdev <samankh.dev@gmail.com>
2021-01-11 08:48:00 +0330

    Created Completed selectJob Component.

--- src/components/selectJobs.vue
+++ src/components/selectJobs.vue
@@ -4,23 +4,29 @@
           <h1>شرکت کننده گان</h1>
       </div>
       <div class="card card-body">
-            <ul class="container-fluid row table-hover">
+            <ul class="container-fluid row">
                 <div class="col col-6">
-                    <li class="text-danger">{{username}}:</li>
+                    <li class="text-danger">
+                        <span class="badge badge-danger">
+                            {{username}}:
+                        </span>
+                    </li>
                 <li v-for="name in players[0]" :key="name.id">
-                    {{name.name}}:
+                    <span class="badge badge-danger">
+                        {{name.name}}:
+                    </span>
 
                 </li>
                 </div>
                 <div class="col col-6">
-                    <li v-for="job in jobs" :key="job">
-                        {{job}}
+                    <li v-for="job in jobs" :key="job.id">
+                        <span :class="`text-${job.color}`">{{job.name}}</span>
                     </li>
                 </div>
             </ul>
       </div>
       <div class="card card-footer">
-          <router-link to="select-jobs" class="btn btn-outline-danger">بعدی</router-link>
+          <router-link to="select-jobs" class="btn btn-outline-danger">شروع</router-link>
       </div>
   </div></template>
 
@@ -29,13 +35,13 @@ export default {
 data(){
     return{
         jobs:[
-        "اسنایپر",
-        "دکتر",
-        "قاضی",
-        "مافیا",
-        "کاراگاه",
-        "شهردار"
-    
+        
+        {id:1,name:"ساقی",color:"danger"},
+        {id:2,name:"دزد",color:"black-50"},
+        {id:3,name:"گودفادر",color:"black"},
+        {id:4,name:"رییس مافیا",color:"warning"},
+        {id:5,name:"کاراگاه",color:"info"},
+        {id:6,name:"شهردار",color:"primary"}
         ],
         players:[],
         username:""
