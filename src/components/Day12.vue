ac7ee864f82878054548ea7ac79b6fd5167b2831
samankhdev <samankh.dev@gmail.com>
2021-01-19 08:30:54 +0330

    Complete Project Education

--- src/components/Day12.vue
+++ src/components/Day12.vue
@@ -2,14 +2,14 @@
 
   <div id="boxSelectUser" class="bg-black">
     <div class="card card-header">
-    <h1 v-html="name"></h1>
+    <h1 v-html="name" ></h1>
     </div>
         <div class="card card-body">
             <span class="w-100 text-right" v-html="message"></span>        
         </div>
-    <button class="btn btn-primary btn-block btn-Choose" id="btn1" v-html="btnOk" @click="btnYes">
+    <button class="btn btn-primary btn-block btn-Choose" id="btn1" v-html="btnOk" @click="btnNO">
     </button>
-    <button class="btn btn-primary btn-block btn-Choose" id="btn2" v-html="btnNo" @click="btnNO">
+    <button class="btn btn-primary btn-block btn-Choose" id="btn2" v-html="btnNo" @click="btnYes">
     </button>
 
 <button class="btn btn-primary btn-block btn-Choose d-none" id="btn3" v-html="btnRes" @click="nextObject">
@@ -19,55 +19,48 @@
 
 <script>
 export default {
-data(){
-    return{
-        name:"روز دوازدهم",
-        btnOk:"باشه قول میدم",
-        btnNo:"نه هرچی میدونم رو میگم",
-        btnRes:"روز بعدی",
-        message:`
-        
-        <h6 class="borderRadius text-white p-2 bg-primary" style="direction:rtl">
-            در بیمارستان شما بهوش آمدی پس از ضربه سختی که خوردی 
-            همان مردی(فرد مافیا) که باهاش درگیر شدی حالا اومده بیمارستان
-            ازت میخواد قول بدی که چیزی به کاراگاه نگی 
-                </h6>
-        `,
-        nextDay:false,
-        status:"bad",
-
-    }
-},
-methods:{
-    nextObject(){
-      
+    data(){
+        return{
+            name:"مافیا بیدار میشود",
+            message:`<p style="direction=rtl;text-align:righ">
+            ...دادگاه برگزار میشود
+            <br>
+            ....قاضی وارد می شود همه قیام کنید
+            <br>
+            شما هم بین شاهد ها هستی میتونی همه چیز رو اقرار کنی و مافیا رو به خاک سیاه بنشونی<span class="badge badge-primary">:راوی</span>
+            <br>
+            قاضی٫کاراگاه٫پلیس٫شهردار مردم همه هستن تا شاهد این اتفاق تاریخی باشند
+            <br>
+            <br>
+            <span class="alert alert-info">
+            دیروز بهت پول زیادی پیشنهاد  دادن که شهادت ندی, اگه قبول کنی از دادگاه بیرون میری    
+            </span>
+            </p>`,
+            btnOk:"پیشنهاد را قبول نمی کنم",
+            btnNo:"قبول میکنم",
+            btnRes:"برو به صفحه اصلی",
+            nextDay:false
+        }
     },
-    btnYes(){
-      
-    },
-    btnNO(){
-        alert();
-        if(this.nextDay==false)
-        {
-            document.querySelector("#btn1").style.display="none";
-            document.querySelector("#btn2").style.display="none";
-            document.querySelector("#btn3").classList.add("d-block");
-            this.message=`
-            کاراگاه ها و پلیس ها پشت در بودن
-            
-            .با سروصدایی که کردی سریع اومدن و اونو گرفتنش
-
-            `;
-        }else{
-
+    methods:{
+        btnNO(){
+            if(this.nextDay==false){
+                this.name="!بوممم مافیا برد";
+                this.message=`
+                <div class="alert alert-danger">
+                <strong>بمب گذار </strong>همه رو داخل دادگاه ترکوند
+                </div>
+                :)دفعه بعدی بیشتر دقت کن یه جا همه جمع نشن
+                `;
+            document.querySelector("#btn1").classList.add("d-none");;
+            document.querySelector("#btn2").classList.add("d-none");;
+            document.querySelector("#btn3").classList.add("d-block");;
+            }
+        },
+        nextObject(){
+            window.location.replace("/");
         }
     }
-},
-created(){
-  
-}
-
-
 }
 </script>
 
--- src/components/ShowNames.vue
+++ src/components/ShowNames.vue
@@ -5,12 +5,26 @@
           <h1>شرکت کننده گان</h1>
       </div>
       <div class="card card-body">
-            <ul>
-                <li class="text-danger">شما:  {{YourName}}</li>
-                <li v-for="name in names" :key="name.id">
-                    {{name.name}}
+            <ul class="">
 
-                </li>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-6">
+                    <div class="row">
+                    <div class="col-12 text-danger">شهروند</div>
+                    <div class="col-12 text-danger" v-for="name in names" :key="name.id">{{name.name}}</div>
+                    
+                    </div>
+                    
+                    </div>
+                    <div class="col-6">
+            <div class="row">
+            
+<div class="col-12 badge badge-danger">شما:{{YourName}}</div>
+                    <div class="col-12 text-danger" v-for="job in jobs" :key="job.id">{{job.name}}</div>
+                    </div>
+                    </div>
+                
+                </div>
             </ul>
       </div>
       <div class="card card-footer">
@@ -22,15 +36,73 @@
 <script>
 export default {
 data(){
+    
     return{
+        YourName:"",
         names:[
             {id:1,name:"حسن مرادی"},
             {id:2,name:"محمد پیوندی"},
             {id:3,name:"سارا میرشکاری"},
-            {id:4,name:"مهدی رسولی"},
-            {id:5,name:"رضا پیوندی"}
+            {id:4,name:"محمدرضا خشیار"},
+            {id:5,name:"رضا پیوندی"},
+            {id:6,name:"مهدی رسولی"},
+            {id:7,name:"صادق محمدی"},
+            {id:8,name:"باقر کشفی"},
+            {id:9,name:"محسن عطار"},
+            {id:10,name:"حمید یزدانپناه"},
+            {id:11,name:"حسین پاکدست"}
+
+            
         
         ],
+        jobs:[
+            {
+                id:1,
+                name:"بمب گذار"
+            },
+            {
+                id:2,
+                name:"رییس مافیا"
+            },
+            {
+                id:3,
+                name:"دزد"
+            },
+            {
+                id:4,
+                name:"قاتل"
+            },
+            {
+                id:5,
+                name:"دکتر"
+            },
+              {
+                id:6,
+                name:"مافیا"
+            },
+              {
+                id:7,
+                name:"دادستان"
+            },
+              {
+                id:8,
+                name:"قاضی"
+            },
+              {
+                id:9,
+                name:"کاراگاه"
+            },
+              {
+                id:10,
+                name:"اسنایپر"
+            },
+              {
+                id:11,
+                name:"وکیل"
+            }
+            
+
+        ]
     }
 },
 methods:{
